82241e45a1c03b4c7155d4f30c86ddf37d101e54
Rudy-009 <rofxnaos@gmail.com>
2025-08-24 16:20:58 +0900

    Migration 프로젝트 생성

---
+++ DispatchQueue/6. sync (Thread Safety)/Thread-Safety/Thread-Safety/GlobalActorDemo.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalActorDemo.swift
+//  Thread-Safety
+//
+//  Created by 이승준 on 8/23/25.
+//
+
+import UIKit
+
+class GlobalActorDemo: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
---
+++ DispatchQueue/8. Migration/Animation_UIKit/Animation_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation_UIKit
+//
+//  Created by 이승준 on 8/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
