841ab9f4efc52e5f18b6c4058ce6a44bb5474001
Rudy-009 <rofxnaos@gmail.com>
2025-10-20 21:30:22 +0900

    테스트용 프로젝트 생성

---
+++ Swift Concurrency/Concurrency-Test-Project/Concurrency-Test-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Concurrency-Test-Project
+//
+//  Created by 이승준 on 10/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
